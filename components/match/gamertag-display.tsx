fa494bf142fc5695e476d5641962ab73fee0c6bf
GravlLift <jason.gravell@gmail.com>
2025-11-05 14:49:32 -0500

    Add bot gamertag handler

--- components/match/gamertag-display.tsx
+++ components/match/gamertag-display.tsx
@@ -17,12 +17,76 @@ export default function GamertagDisplay({
   return (
     <Flex>
       <Box>
-        {player.gamertag ? (
-          <Link asChild cursor={'pointer'}>
-            <NextLink prefetch={false} href={'/players/' + player.gamertag}>
-              {player.gamertag}
-            </NextLink>
-          </Link>
+        {player.PlayerId?.startsWith('bid') ? (
+          <>Bot</>
+        ) : player.gamertag ? (
+          <>
+            <Link asChild cursor={'pointer'}>
+              <NextLink prefetch={false} href={'/players/' + player.gamertag}>
+                {player.gamertag}
+              </NextLink>
+            </Link>
+            {matchId && fileHandleRef && (
+              <Testing>
+                <Box hideBelow="lg" pl={1}>
+                  {player.gamertag !== 'GravlLift' ? (
+                    <Ban
+                      cursor={'pointer'}
+                      onClick={async () => {
+                        if (!player.PlayerId) return;
+
+                        if (fileHandleRef.current == null) {
+                          try {
+                            const [newHandle] = await showOpenFilePicker({
+                              id: 'blacklist',
+                            } as Parameters<typeof showOpenFilePicker>[0]);
+
+                            await newHandle.requestPermission({
+                              mode: 'readwrite',
+                            });
+                            fileHandleRef.current = newHandle;
+                          } catch (e) {
+                            return;
+                          }
+                        }
+
+                        const fileData = await fileHandleRef.current.getFile();
+                        const blackList: Record<
+                          string,
+                          {
+                            matchId?: string;
+                            reason?: string;
+                            gamertag?: string;
+                          }
+                        > = JSON.parse(await fileData.text());
+
+                        const reason = window.prompt('Blacklist reason');
+
+                        if (reason == null) {
+                          return;
+                        }
+
+                        blackList[player.PlayerId] = {
+                          gamertag: player.gamertag,
+                          matchId,
+                        };
+                        if (reason) {
+                          blackList[player.PlayerId].reason = reason;
+                        }
+
+                        const writable =
+                          await fileHandleRef.current.createWritable();
+                        await writable.write(
+                          JSON.stringify(blackList, null, 2)
+                        );
+                        await writable.close();
+                      }}
+                    />
+                  ) : null}
+                </Box>
+              </Testing>
+            )}
+          </>
         ) : (
           <Skeleton
             height="20px"
@@ -32,59 +96,6 @@ export default function GamertagDisplay({
           />
         )}
       </Box>
-      {matchId && fileHandleRef && (
-        <Testing>
-          <Box hideBelow="lg" pl={1}>
-            {player.gamertag !== 'GravlLift' ? (
-              <Ban
-                cursor={'pointer'}
-                onClick={async () => {
-                  if (!player.PlayerId) return;
-
-                  if (fileHandleRef.current == null) {
-                    try {
-                      const [newHandle] = await showOpenFilePicker({
-                        id: 'blacklist',
-                      } as Parameters<typeof showOpenFilePicker>[0]);
-
-                      await newHandle.requestPermission({
-                        mode: 'readwrite',
-                      });
-                      fileHandleRef.current = newHandle;
-                    } catch (e) {
-                      return;
-                    }
-                  }
-
-                  const fileData = await fileHandleRef.current.getFile();
-                  const blackList: Record<
-                    string,
-                    { matchId?: string; reason?: string; gamertag?: string }
-                  > = JSON.parse(await fileData.text());
-
-                  const reason = window.prompt('Blacklist reason');
-
-                  if (reason == null) {
-                    return;
-                  }
-
-                  blackList[player.PlayerId] = {
-                    gamertag: player.gamertag,
-                    matchId,
-                  };
-                  if (reason) {
-                    blackList[player.PlayerId].reason = reason;
-                  }
-
-                  const writable = await fileHandleRef.current.createWritable();
-                  await writable.write(JSON.stringify(blackList, null, 2));
-                  await writable.close();
-                }}
-              />
-            ) : null}
-          </Box>
-        </Testing>
-      )}
     </Flex>
   );
 }
