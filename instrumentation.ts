6e961370c5a48693c1e1290e30cfc8eed7e3aa3b
GravlLift <jason.gravell@gmail.com>
2025-11-10 16:29:50 -0500

    Add proxy support

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,10 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./lib/application-insights/server');
+
+    if (process.env.HTTP_PROXY) {
+      const undici = await import('undici');
+      undici.setGlobalDispatcher(new undici.ProxyAgent(process.env.HTTP_PROXY));
+    }
   }
 }
