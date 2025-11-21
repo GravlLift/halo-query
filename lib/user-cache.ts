128b1fc2f6275f33e954dd1d5c72eca60e2fc957
GravlLift <jason.gravell@gmail.com>
2025-11-20 23:04:14 -0500

    Make redis optional

--- lib/user-cache.ts
+++ lib/user-cache.ts
@@ -3,16 +3,28 @@ import { Redis } from '@upstash/redis';
 import { wrapXuid } from '@gravllift/halo-helpers';
 import { DateTime } from 'luxon';
 
-const redis = new Redis({
-  url: process.env['KV_REST_API_URL'] ?? process.env['UPSTASH_REDIS_REST_URL'],
-  token:
-    process.env['KV_REST_API_TOKEN'] ?? process.env['UPSTASH_REDIS_REST_TOKEN'],
-  keepAlive: false,
-});
+const url =
+  process.env['KV_REST_API_URL'] || process.env['UPSTASH_REDIS_REST_URL'];
+const token =
+  process.env['KV_REST_API_TOKEN'] || process.env['UPSTASH_REDIS_REST_TOKEN'];
+let redis: Redis | undefined;
+if (!url || !token) {
+  redis = new Redis({
+    url,
+    token,
+    keepAlive: false,
+  });
+} else {
+  redis = undefined;
+}
 
 export function getByXuid(
   xuids: string[]
 ): Map<string, Promise<UserInfo | null>> {
+  if (!redis) {
+    return new Map(xuids.map((xuid) => [xuid, Promise.resolve(null)]));
+  }
+
   const mGetPromise = redis.mget<UserInfo[]>(
     xuids.map((xuid) => wrapXuid(xuid))
   );
@@ -29,6 +41,12 @@ export function getByXuid(
 export function getByGamertag(
   gamertags: string[]
 ): Map<string, Promise<UserInfo | null>> {
+  if (!redis) {
+    return new Map(
+      gamertags.map((gamertag) => [gamertag, Promise.resolve(null)])
+    );
+  }
+
   return new Map(
     gamertags.map((gamertag) => {
       const key = /gt\([^)]+\)/.test(gamertag)
@@ -40,6 +58,10 @@ export function getByGamertag(
 }
 
 export async function addUserInfo(user: UserInfo): Promise<void> {
+  if (!redis) {
+    return;
+  }
+
   const expireAt = DateTime.now().plus({
     days: +(process.env.USER_CACHE_EXPIRATION_DAYS || 7),
   });
