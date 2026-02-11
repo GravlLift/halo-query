eebc6e5487c7d900de8fafb80f2cb3b4ae70fccd
GravlLift <jason.gravell@gmail.com>
2026-02-11 00:30:58 -0500

    Adjust CORS settings

--- app/oauth2/callback/page.tsx
+++ app/oauth2/callback/page.tsx
@@ -2,11 +2,14 @@
 import { useEffect } from 'react';
 import { Loading } from '../../../components/loading';
 import { useAuthentication } from '../../../lib/contexts/authentication-contexts';
+import { redirect } from 'next/navigation';
 
 export default function Callback() {
   const { acquireOauth2AccessToken } = useAuthentication();
   useEffect(() => {
-    acquireOauth2AccessToken();
+    acquireOauth2AccessToken().then(() => {
+      redirect('/');
+    });
   }, []);
   return <Loading centerProps={{ height: '100vh' }} />;
 }
--- next.config.js
+++ next.config.js
@@ -11,6 +11,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  headers: async () => {
+    return [
+      {
+        source: '/proxy/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          { key: 'Access-Control-Max-Age', value: '86400' },
+        ],
+      },
+    ];
+  },
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
   },
