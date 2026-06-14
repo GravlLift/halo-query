03e8582fcff37706b67e3fd99608d9a415a1097c
Jason Gravell <cr239651@charlesriverlabs.com>
2026-06-14 13:20:53 -0500

    Player fetcher

--- app/graphql/route.ts
+++ app/graphql/route.ts
@@ -1,15 +1,70 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import type { IResolvers } from '@graphql-tools/utils';
+import {
+  isXuid,
+  SkillProp,
+  HaloCaches,
+  getByXuid,
+} from '@gravllift/halo-helpers';
+import {
+  HaloInfiniteClient,
+  StaticXstsTicketTokenSpartanTokenProvider,
+  XboxClient,
+} from 'halo-infinite-api';
 import type { NextRequest } from 'next/server';
 import { provider } from '../../lib/leaderboard/sqlite';
 import typeDefs from './schema.graphql';
-import { SkillProp } from '@gravllift/halo-helpers';
 
-const resolvers: IResolvers = {
+let playlistIdsPromise: Promise<string[]> | null = null;
+function getPlaylistIds() {
+  playlistIdsPromise ??= provider.getPlaylistAssetIds();
+  return playlistIdsPromise;
+}
+
+let haloInfiniteClient: HaloInfiniteClient | null = null;
+function getHaloInfiniteClient(request: NextRequest) {
+  haloInfiniteClient ??= new HaloInfiniteClient(
+    new StaticXstsTicketTokenSpartanTokenProvider(
+      request.headers.get('x-343-spartan-token')!
+    )
+  );
+  return haloInfiniteClient;
+}
+
+let xboxClient: XboxClient | null = null;
+function getXboxClient(request: NextRequest) {
+  xboxClient ??= new XboxClient({
+    getXboxLiveV3Token: async () => request.headers.get('authorization')!,
+    clearXboxLiveV3Token: async () => {
+      throw new Error('Clearing xbox live token is not supported');
+    },
+  });
+  return xboxClient;
+}
+
+let haloCache: HaloCaches | null = null;
+function getHaloCaches(request: NextRequest) {
+  haloCache ??= new HaloCaches(
+    getHaloInfiniteClient(request),
+    getXboxClient(request),
+    {
+      xuidIsCurrentUser: async (xuid: string) => {
+        // your implementation here
+        return false;
+      },
+      additionalXuidFetcher: {
+        fetchMapFn: getByXuid,
+      },
+    }
+  );
+  return haloCache;
+}
+
+const resolvers: IResolvers<any, NextRequest> = {
   Query: {
-    playlistIds: () => provider.getPlaylistAssetIds(),
-    leaderboard: (
+    playlistIds: () => getPlaylistIds(),
+    leaderboard: async (
       _: unknown,
       {
         playlistId,
@@ -18,10 +73,25 @@ const resolvers: IResolvers = {
         playlistId: string;
         skillProp: SkillProp;
       }
-    ) => ({
-      playlistId,
-      skillProp,
-    }),
+    ) => {
+      const playlistIds = await getPlaylistIds();
+      if (!playlistIds.includes(playlistId)) {
+        return null;
+      }
+      return {
+        playlistId,
+        skillProp,
+      };
+    },
+    player: async (
+      _,
+      { xuidOrGamertag }: { xuidOrGamertag: string },
+      context
+    ) => {
+      const haloCache = getHaloCaches(context);
+      const user = await haloCache.usersCache.get(xuidOrGamertag);
+      return user;
+    },
   },
   Leaderboard: {
     count: ({ playlistId }: { playlistId: string }) =>
@@ -55,9 +125,13 @@ const resolvers: IResolvers = {
   },
 };
 
-const handler = startServerAndCreateNextHandler<NextRequest>(
-  new ApolloServer({ typeDefs, resolvers }),
-  {}
+const handler = startServerAndCreateNextHandler<NextRequest, NextRequest>(
+  new ApolloServer<NextRequest>({ typeDefs, resolvers }),
+  {
+    context: async (request) => {
+      return request;
+    },
+  }
 );
 
 export async function GET(request: NextRequest) {
